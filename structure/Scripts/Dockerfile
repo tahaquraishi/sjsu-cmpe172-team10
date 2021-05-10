2b50f11b7545e39e144c39cffea7d935dd7bef99
Taha Quraishi <42878753+tahaquraishi@users.noreply.github.com>
2021-05-10 14:50:57 -0700

    Add files via upload

---
+++ structure/Scripts/Dockerfile
@@ -0,0 +1,4 @@
+FROM openjdk:latest
+ADD target/cmpe172project.jar app.jar
+ENTRYPOINT ["java","-jar","app.jar"]
+EXPOSE 8080
---
+++ structure/Scripts/data.sql
@@ -0,0 +1,18 @@
+# USE tlc;
+
+# INSERT INTO employees (name, scheduled, working) VALUES ('Taha Quraishi', true, true);		# employee 1
+# INSERT INTO employees (name, scheduled, working) VALUES ('Jevvvansh Lota', false, false);		# employee 2
+# INSERT INTO employees (name, scheduled, working) VALUES ('Simran Bains', true, false);		# employee 3
+# INSERT INTO employees (name, scheduled, working) VALUES ('Jashan Dhillon', false, false);		# employee 4
+# INSERT INTO employees (name, scheduled, working) VALUES ('Martin Vlad', true, false);			# employee 5
+# INSERT INTO employees (name, scheduled, working) VALUES ('Roei Burstein', true, true);		# employee 6
+# INSERT INTO employees (name, scheduled, working) VALUES ('Mani Movva', true, false);			# employee 7
+# INSERT INTO employees (name, scheduled, working) VALUES ('Atharva Shende', true, false);		# employee 8
+# INSERT INTO employees (name, scheduled, working) VALUES ('Brandon Cheu', true, true);			# employee 9
+# INSERT INTO employees (name, scheduled, working) VALUES ('Alan Le', false, true);				# employee 10
+# INSERT INTO employees (name, scheduled, working) VALUES ('Michael Sandell', true, true);		# employee 11
+# INSERT INTO employees (name, scheduled, working) VALUES ('Lucille Hamilton', false, false);	# employee 12
+# INSERT INTO employees (name, scheduled, working) VALUES ('Darleen Hamm', true, false);		# employee 13
+# INSERT INTO employees (name, scheduled, working) VALUES ('Daniel Guillory', false, false);	# employee 14
+# INSERT INTO employees (name, scheduled, working) VALUES ('Kathy Johnson', true, false);		# employee 15
+# INSERT INTO employees (name, scheduled, working) VALUES ('Raif Qazi', false, false);		    # employee 16
